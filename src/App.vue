568e629ba37b0a29e6392fefc20d4431ffe6a988
Anna Vasileva <vasilec1992@yandex.rum>
2022-02-25 14:11:37 +0100

    1 lesson

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Calcul msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import Calcul from "./components/Calcul.vue";
+
+export default {
+  name: "App",
+  components: {
+    Calcul,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Calcul.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="display">
+      <input v-model.number="operand1" type="number" />
+      <input v-model.number="operand2" type="number" />
+      = {{ result }}
+    </div>
+    <div class="keyboard">
+      <button @click="sum(operand1 + operand2)">+</button>
+      <button @click="minus(operand1, operand2)">-</button>
+      <button @click="divide(operand1, operand2)">/</button>
+      <button @click="multiply(operand1, operand2)">*</button>
+      <button @click="power(operand1, operand2)">^</button>
+      <button @click="divideModal(operand1, operand2)">%</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calcul",
+  data() {
+    return {
+      operand1: "",
+      operand2: "",
+      result: 0,
+      error: "The division by zero is forbidden",
+    };
+  },
+  methods: {
+    sum(op1, op2) {
+      this.result = op1 + op2;
+    },
+    divide(op1, op2) {
+      if (op2 == 0) {
+        this.result = this.error;
+      } else {
+        this.result = op1 / op2;
+      }
+    },
+    minus(op1, op2) {
+      this.result = op1 - op2;
+    },
+    multiply(op1, op2) {
+      this.result = op1 * op2;
+    },
+    divideModal(op1, op2) {
+      this.result = op1 % op2;
+    },
+    power(op1, op2) {
+      this.result = Math.pow(op1, op2);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
